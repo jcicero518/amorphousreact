38e292283fd36b6aebf16fc8632d7541f7e4753e
Jeff Cicero <jeff.cicero@zone5.com>
2017-10-04 19:24:28 -0400

    Update

--- wp-content/themes/amorphous/category.php
+++ wp-content/themes/amorphous/category.php
@@ -11,60 +11,67 @@ get_header(); ?>
 
 	<section class="section">
 		<div class="container">
-
-			<div class="box">
-				<?php
-				global $wp_query;
-				$args = array_merge( $wp_query->query_vars, [
-					'post_type' => 'card',
-					'posts_per_page' => 10
-				]);
-				$categoryQuery = new WP_Query( $args );
-				if ( $categoryQuery->have_posts() ) : ?>
-
-					<header class="page-header">
+			<div class="columns">
+				<div class="column is-two-thirds">
+					<div class="box">
 						<?php
+						global $wp_query;
+						$args = array_merge( $wp_query->query_vars, [
+							'post_type' => 'card',
+							'posts_per_page' => 10
+						]);
+						$categoryQuery = new WP_Query( $args );
+						if ( $categoryQuery->have_posts() ) : ?>
 
-						the_archive_title( '<h1 class="title">', '</h1>' );
-						the_archive_description( '<div class="archive-description">', '</div>' );
-						?>
-					</header><!-- .page-header -->
-
-					<?php
-					/* Start the Loop */
-					while ( $categoryQuery->have_posts() ) : $categoryQuery->the_post();
-
-						?>
-						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<header class="entry-header">
-								<h2 class="title">
-									<a href="<?= esc_url( get_permalink( $categoryQuery->post->ID ) ); ?>"><?= get_the_title( $categoryQuery->post->ID); ?></a>
-								</h2>
+							<header class="page-header">
 								<?php
-								if ( 'post' === get_post_type() || 'card' === get_post_type() ) : ?>
-									<div class="entry-meta">
-										<span class="tag is-info">Topics</span> <?= get_the_category_list( ', ' ); ?>
-									</div>
-									<?php
-								endif;
+
+								the_archive_title( '<h1 class="title">', '</h1>' );
+								the_archive_description( '<div class="archive-description">', '</div>' );
 								?>
-							</header>
-							<div class="entry-content">
-								<?= get_the_excerpt( $categoryQuery->post->ID ); ?>
-							</div>
-						</article>
+							</header><!-- .page-header -->
 
-					<?php
-					endwhile;
+							<?php
+							/* Start the Loop */
+							while ( $categoryQuery->have_posts() ) : $categoryQuery->the_post();
 
-					the_posts_navigation();
+								?>
+								<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<header class="entry-header">
+										<h2 class="title">
+											<a href="<?= esc_url( get_permalink( $categoryQuery->post->ID ) ); ?>"><?= get_the_title( $categoryQuery->post->ID); ?></a>
+										</h2>
+										<?php
+										if ( 'post' === get_post_type() || 'card' === get_post_type() ) : ?>
+											<div class="entry-meta">
+												<span class="tag is-info">Topics</span> <?= get_the_category_list( ', ' ); ?>
+											</div>
+											<?php
+										endif;
+										?>
+									</header>
+									<div class="entry-content">
+										<?= get_the_excerpt( $categoryQuery->post->ID ); ?>
+									</div>
+								</article>
 
-				endif;
-				wp_reset_postdata();
-				?>
+							<?php
+							endwhile;
+
+							the_posts_navigation();
+
+						endif;
+						wp_reset_postdata();
+						?>
+					</div>
+				</div>
+				<div class="column">
+					<div class="box">
+						<?php get_sidebar(); ?>
+					</div>
+				</div>
 			</div>
-
-
+			
 		</div><!-- #main -->
 	</section><!-- #primary -->
 
