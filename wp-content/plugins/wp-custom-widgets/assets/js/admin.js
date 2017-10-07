2d07dc6670c91e03cf52b1fb8985408349df5419
Jeff Cicero <jeff.cicero@zone5.com>
2017-10-06 21:08:37 -0400

    update

--- wp-content/plugins/wp-custom-widgets/assets/js/admin.js
+++ wp-content/plugins/wp-custom-widgets/assets/js/admin.js
@@ -1,24 +1,41 @@
 (function($) {
-    var attrs = {};
+    var ourWidgetAttrs = {
+        id_base: 'related_links_widget'
+    };
+    var ourWidgetFlags = {
+        is_open: false,
+        is_ours: false
+    };
     var container = document.querySelector( '.wp-custom-widgets-related-wrapper' );
     var widgetAttrsElem = container.querySelector( '.is-hidden-widget-attrs' );
     var addButton = container.querySelector( '.button-add-row' );
 
-    function setAttrs() {
-        var data = widgetAttrsElem.dataset;
-        console.log(data, 'data');
-    }
-    addButton.addEventListener( 'click', function( event ) {
-        console.log(event, 'event');
-        setAttrs();
-    });
+    function locateToggledWidget( $toggleTarget ) {
+        var
+            $widgetInside,
+            $toggleBtn,
+            $thisWidget = $toggleTarget.closest( '.widget' );
 
-    setAttrs();
+        $toggleBtn = $thisWidget.find( '.widget-top button.widget-action' );
+        ourWidgetFlags.is_open = $toggleBtn.parents( '.widget' ).hasClass( 'open' );
+
+        $widgetInside = $thisWidget.find( '.widget-inside' );
+        ourWidgetFlags.is_ours = ( $widgetInside.find( 'input[name="id_base"]' ).val() === ourWidgetAttrs.id_base );
+    }
+
+    function isReady() {
+        return ( ourWidgetFlags.is_open && ourWidgetFlags.is_ours );
+    }
+
     // https://core.trac.wordpress.org/browser/branches/4.8/src/wp-admin/js/widgets.js
     $(function() {
         $(document).on('click.widgets-toggle', function( event ) {
-           console.log(event, 'postbox ');
+            console.log(event, 'toggle widget open / closed ');
+            var $target = $( event.target );
+            locateToggledWidget( $target );
+            console.log(isReady(), 'isOurs');
         });
+
     })
 
 })(jQuery);
